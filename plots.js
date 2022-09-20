ee73bc8ea7a1a6956aa402784bc58231f69a0eb4
Haynn Tang <thanatoscharun@gmail.com>
2022-09-20 14:46:56 -0600

    updated

--- plots.js
+++ plots.js
@@ -1,7 +1,7 @@
 function init() {
   var selector = d3.select("#selDataset");
 
-  d3.json("data/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     console.log(data);
     var sampleNames = data.names;
     sampleNames.forEach((sample) => {
@@ -10,6 +10,30 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
+    buildcharts(sampleNames[0])
 })}
 
 init();
+
+
+
+function buildcharts(sample) {
+  d3.json("samples.json").then((data) => {
+    // filter data
+    var metadatainfo = data.metadata.filter( x => x.id == sample);
+    // console.log(metadatainfo[0]);
+    // console.log(metadatainfo);
+    var metadatasample = metadatainfo[0];
+    // var washfreq = 
+
+    var sampleinfo = data.samples.filter( x => x.id == sample);
+    // console.log(samplesample[0]);
+    
+    var samplesample = sampleinfo[0];
+    console.log(samplesample);
+    var otuid = samplesample.otu_ids;
+    var otulabel= samplesample.otu_labels;
+    var samplevalue = samplesample.sample_values;
+
+  });
+};
